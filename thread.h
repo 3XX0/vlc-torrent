bf0548ed5e65359d8751fb67b17f165d308aad40
Jonathan Calmels <jbjcalmels@gmail.com>
2015-01-24 16:20:33 +0100

    Remove pragma once

--- thread.h
+++ thread.h
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#pragma once
-
 #include <mutex>
 #include <functional>
 
